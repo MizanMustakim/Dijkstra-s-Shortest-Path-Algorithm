a4f4e633ccd1e4bc7e494597ce618da252738450
Md Mizanur Rahman Mustakim <MizanMustakim@users.noreply.github.com>
2021-02-12 02:09:26 +0600

    update

--- Shortest Path in 2D model/main.py
+++ Shortest Path in 2D model/main.py
@@ -1,10 +1,19 @@
-from shortPathFinder import DijkstraAlgorithm
+import DijkstraAlgo as da
 from Graph import graph
 
 def main():
-    x = DijkstraAlgorithm()
-    source = int(input("Enter the source: "))   # Take input of the source value
-    x.dijkstra(graph, source)
+    x = da.DijkstraAlgorithm()
+    source = int(input("\nEnter the source: "))   # Take input of the source value
+    destination = int(input("Enter the destination: "))
+    x.dijkstraWithPath(graph, source, destination)
+
+    shortest_path = x.path()
+    distance = x.distance()
+
+    print("\nThe shortest route: ")
+    print(*shortest_path)   #It will print the path
+    print("The shortest distance is {:.3f}".format(*distance))         #It will print the distance
+
 
 if __name__ == '__main__':
     main()
